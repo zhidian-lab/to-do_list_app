37eb2c2201c090e076e8f82bdec98551619d7911
zhidian-lab <1409625488@qq.com>
2024-04-20 12:18:43 +0800

    complete the cli app

--- functions.py
+++ functions.py
@@ -1,5 +1,15 @@
 FILE_NAME = "to-dos.txt"
-def get_action():
+def get_action(file_path=FILE_NAME):
     with open(FILE_NAME, 'r') as file:
-        print(file.readlines)
-get_action()
+        todos_local = file.readlines()
+    return todos_local
+def add_action(todo_arg, file_path=FILE_NAME):
+    with open(FILE_NAME, 'a') as file:
+        file.writelines(todo_arg)
+def overwrite_action(new_todos, file_path=FILE_NAME):
+    with open(FILE_NAME, 'w') as file:
+        file.writelines(new_todos)
+if __name__ == "__main__":
+    add_action('test3\n')
+    overwrite_action(["test1new\n"])
+    print(get_action())
